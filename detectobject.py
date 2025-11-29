0c7199a17b20fc36a52329ab3968aafb2a755520
elan2006 <elan@thaalai.com>
2025-11-29 14:27:06 +0530

    add flags and modularize code

--- detectobject.py
+++ detectobject.py
@@ -3,15 +3,13 @@ from ultralytics import YOLO
 import sys
 import os
 import ffmpeg
+import argparse
 
 
-def main():
-    video_file = sys.argv[1]
+def object_detection(input_video_filepath, output_video_filepath) -> str:
     model = YOLO("yolov8n.pt")
-
-    input_video = video_file
-    temp_video = "temp.mp4"
-    output_video = "output.mp4"
+    input_video = input_video_filepath
+    temp_video = output_video_filepath
 
 
     cap = cv.VideoCapture(input_video)
@@ -23,7 +21,7 @@ def main():
     fourcc = cv.VideoWriter_fourcc(*"mp4v")
     out = cv.VideoWriter(temp_video, fourcc, fps, (width, height))
 
-    print("Starting to writes file....")
+    print("Starting to write file....")
 
     while True:
         ret, frame = cap.read()
@@ -37,9 +35,13 @@ def main():
 
     cap.release()
     out.release()
-    print("Video processed. Merging audio with ffmpeg-python...")
-    video_stream = ffmpeg.input(temp_video)
-    audio_stream = ffmpeg.input(input_video)
+
+    return temp_video
+
+
+def copy_audio_to_output(input_video, video_with_audio, output_video):
+    video_stream = ffmpeg.input(input_video)
+    audio_stream = ffmpeg.input(video_with_audio)
 
     (
         ffmpeg
@@ -51,12 +53,36 @@ def main():
             acodec="copy"
         )
         .overwrite_output()
-        .run()
+        .run(quiet=True)
     )
 
 
+
+def main():
+
+    parser = argparse.ArgumentParser(
+        description="Run object detection and store it in a video file."
+    )
+    parser.add_argument("--input-video",
+                        "-i",
+                        help="Path to the input video file")
+    parser.add_argument("--output-video",
+                        "-o",
+                        default="output.mkv",
+                        help="Path to save the processed output video")
+    parser.add_argument("--temp-video",
+                        "-t",
+                        default="temp.mkv",
+                        help="Store a temporary video for further processing")
+
+    args = parser.parse_args()
+
+    input_video = args.input_video
+    output_video = args.output_video
+    temp_video = object_detection(input_video, "temp.mkv")
+
+    copy_audio_to_output(input_video=temp_video, video_with_audio=input_video, output_video=output_video)
     print("Done! Saved as:", output_video)
-    
 
 
 if __name__ == "__main__":
